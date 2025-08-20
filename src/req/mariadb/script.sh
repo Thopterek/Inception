b77eac9bdfda869ced2d1711e56ad9f47ab45b2a
Nataniel Dziadzia <sumplous.sumplous@gmail.com>
2025-08-20 15:42:34 +0200

    back to mariadb problems time to debug the script

--- src/req/mariadb/script.sh
+++ src/req/mariadb/script.sh
@@ -10,31 +10,44 @@ FILE=/etc/my.cnf
 # as per sed [options] ['command/pattern/replace/flag'] [filename]
 sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' "$FILE"
 
+cat /etc/my.cnf
+echo "was it huge?"
+
+# ------------------
+# DISABLED FOR DEBUG
+# just mariadb-safe
+# ------------------
 # start the mariaDB in safe mode and disable netwoking
 # running it in the background through usage of &
-mariadbd-safe --skip-networking &
+# mariadbd-safe --skip-networking &
+mariadbd-safe --datadir=/var/lib/mysql &
+echo "MariaDB started -> mariadbd-safe exit value -> $?"
 
 # creating a polling logic before the healthcheck
 # works until the mariaDB is ready to accept connection
 # runs every second till returns 0
 until /usr/bin/mariadb-admin ping --silent; do
+	echo "WE ARE PINGING" && \
 	sleep 1
 done
+echo "after the pinging exit value -> $?"
 
 # creating of actual db and the user through SQL syntax
 # most of them are self explanatory through naming
 # flushing the privileges mean that changes take effect
 mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' INDENTIFIED BY '${MYSQL_PASS}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
+echo "what about mysql -u root exit value $?"
 
 # shuts down the mariaDB to restart with all the changes
 # afterwards it will run with networking enabled and in foreground
 # so it will have the fake PID1 from the container
 /usr/bin/mariadb-admin shutdown --socket=/var/run/mysqld/mysqld.sock -u root
+echo "how is the shutdown going exit value $?"
 
 # replacing the shell with mysqld process
 # making the change into PID1 as per above
--- src/req/wordpress/script.sh
+++ src/req/wordpress/script.sh
@@ -4,19 +4,37 @@
 set -e
 
 # prevents wordpress cli from sending emails
+# and adding higher memory limit to PHP
 export WP_CLI_DISABLE_MAIL=true
+export WP_CLI_PHP_ARGS='-d memory_limit=512M'
 
+# ------------------------------------
+# Moving this part inside docker build
+# ------------------------------------
 # check if wordpress CLI is installed
 # if not download, makes executable and moves
-if ! command -v wp > /dev/null; then
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	chmod +x wp-cli.phar
-	mv wp-cli.phar /usr/local/bin/wp
-fi
+# if ! command -v wp > /dev/null; then
+# 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# 	chmod +x wp-cli.phar
+# 	mv wp-cli.phar /usr/local/bin/wp
+# fi
+# echo "Did we fail the command -v -> $?"
+
+# ---------------
+# DEBUG STATEMENT
+# ---------------
+ls -la
+echo "FIRST LISTING"
 
 # making all the important files readable
 cd /var/www/html
 chmod -R 755 /var/www/html
+
+# ---------------
+# DEBUG STATEMENT
+# ---------------
+ls -la
+echo "SECOND LISTING"
 
 # [file name part just linke in DB setup]
 FILE=/etc/php84/php-fpm.d/www.conf
@@ -26,49 +44,71 @@ sed -i 's|^listen =.*|listen = 9000|' "$FILE"
 
 # ------------------------------------------
 # seems like something is wrong at this part
+# adding it for testing right now again
 # ------------------------------------------
 # checking the MariaDB if its ready
 # trying up to 10 times every 2 seconds
 # most likely to be removed if healthcheck works
-# for i in {1..10}; do
-# 	if mariadb -h mariadb -P 3306 -u "${MYSQL_USER}" \
-# 		-p"${MYSQL_PASS}" -e "SELECT 1" &>/dev/null; then
-# 		break
-# 	else
-# 		echo "we wait if MariaDB is ready (${i}/10)"
-# 		sleep 2
-# 	fi
-# done
+for i in {1..10}; do
+	if mariadb -h mariadb -p 3306 -u "${MYSQL_USER}" \
+		-p"${MYSQL_PASS}" -e "SELECT 1" &>/dev/null; then
+		break
+	else
+		echo "we wait if MariaDB is ready (${i}/10)"
+		sleep 2
+	fi
+done
 
 # checking if the WordPress is present if not download
 if [ ! -f index.php ]; then
+	echo "Downloading WordPress" && \
 	wp core download --allow-root
 fi
-
-# values to be filled out in the config file
-VALUES="--dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASS}""
+echo "Was the last command succefull -> $?"
 
 # make a WordPress config file if it doesn't exist
-if [ ! -f wp-config.php ]; then
-	wp config create "$VALUES" --dbhost="mariadb:3306" --allow-root
-fi
+# adding a wraper to try and retry the connection
+# before fully exiting the script
+for i in {1..10}; do
+	if [ ! -f wp-config.php ]; then
+		echo "Creating a wp-config file tried ${i}/10"
+		if wp config create \
+			--dbname="${MYSQL_DATABASE}" \
+			--dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASS}" \
+			--dbhost="mariadb:3306" --allow-root; then
+			echo "WE DID IT"
+			break
+		else
+			echo "we are retrying the config"
+			sleep 2
+		fi
+	else
+		echo "wp-config exists skipping the creation"
+		break
+	fi
+done
+echo "Was the last command succefull -> $?"
 
 # install wordpress if its not there
 # fillout all of the informations about it
 # with supression of warning email not found
 # last line would be replacing the --skip-email
 if ! wp core is-installed --allow-root; then
+	echo "trying to install wordpress and set it up" && \
 	wp core install --url="${DOMAIN_NAME}" --title="${WP_TITLE}" \
 		--admin_user="${WP_ADMIN}" --admin_password="${WP_ADMIN_PASS}" \
 		--admin_email="${WP_ADMIN_EMAIL}" --skip-email --allow-root
 		# 2> >(grep -v '/usr/sbin/sendmail: not found' >&2)
 fi
+echo "Was the last command succefull -> $?"
 
 # create a user if he doesn't exist
 if ! wp user get "${WP_USER}" --allow-root &>/dev/null; then
+	echo "adding a user to wordpress"
 	wp user create "${WP_USER}" "${WP_USER_EMAIL}" \
 		--user_pass="${WP_USER_PASS}" --allow-root
 fi
+echo "Was the last command succefull -> $?"
 
 # setup permissions for the www-data
 chown -R www-data:www-data /var/www/html
