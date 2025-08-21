d4bedbfc68e4b6dae1ec6638f44774b03d789948
Nataniel Dziadzia <sumplous.sumplous@gmail.com>
2025-08-21 15:14:18 +0200

    We made a move to Debian

--- src/req/mariadb/script.sh
+++ src/req/mariadb/script.sh
@@ -3,21 +3,30 @@
 # exiting the script if any command fails
 set -e
 
+# -------------------------------
+# Seems to be not needed for now
+# ------------------------------
 # extra part for permissions
 # adding daemon run ones
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
+# if [ ! -d "/run/mysqld" ]; then
+# 	mkdir -p /run/mysqld
+# 	chown -R mysql:mysql /run/mysqld
+# fi
 
+# --------------------------------------------
+# Old file path used before changing to Debian
+# with below one that is about the new one
+# --------------------------------------------
 # [filename] part that is going to be used
-FILE=/etc/my.cnf
+# FILE=/etc/my.cnf
+FILE=/etc/mysql/mariadb.conf.d/50-server.cnf
+
 
 # using sed: stream editor to find and replace
 # as per sed [options] ['command/pattern/replace/flag'] [filename]
 sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' "$FILE"
 
-cat /etc/my.cnf
+cat $FILE
 echo "was it huge?"
 
 # -------------------
@@ -31,25 +40,38 @@ echo "was it huge?"
 # mariadbd-safe --skip-networking &
 #mariadbd-safe --datadir=/var/lib/mysql &
 #echo "MariaDB started -> mariadbd-safe exit value -> $?"
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	echo "MARIADB GETTING INSTALED"
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql --rpm
-fi
+# -------------------------------------------------------
+# This part generally seems to be not needed as for now
+# because of the change to the Debian version
+# ------------------------------------------------------
+#if [ ! -d "/var/lib/mysql/mysql" ]; then
+#	echo "MARIADB GETTING INSTALED"
+#	mysql_install_db --user=mysql --datadir=/var/lib/mysql --rpm
+#fi
 
-echo "once again background running MariaDB"
+# -------------------------------------------
+# Same part replacments back mysqld_safe
+# easier with debian 
+# the alpine thing is left to show the pain
+# ------------------------------------------
+#echo "once again background running MariaDB"
 # mysqld is going oooout
-/usr/bin/mariadbd --user=mysql --skip-networking &
-echo "successfull mysqld? -> $?"
-MYSQL_PID=$!
+#/usr/bin/mariadbd --user=mysql --skip-networking &
+#echo "successfull mysqld? -> $?"
+#MYSQL_PID=$!
+mysqld_safe --skip-networking &
 
 # creating a polling logic before the healthcheck
 # works until the mariaDB is ready to accept connection
 # runs every second till returns 0
-until /usr/bin/mariadb-admin ping --silent; do
+# -------------------------------------------------------------
+# Replacing the: until /usr/bin/mariadb-admin ping --silent; do
+# with the simpler version for the Debian which is below
+# -------------------------------------------------------------
+until mysqladmin ping --silent; do
 	echo "WE ARE PINGING"
 	sleep 1
 done
-echo "after the pinging exit value -> $?"
 
 echo "we want to use database name -> $MYSQL_DATABASE"
 echo "user -> $MYSQL_USER with password -> $MYSQL_PASS"
@@ -64,12 +86,17 @@ CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-echo "what about mysql -u root exit value $?"
+
+# echo "what about mysql -u root exit value $?"
 
 # shuts down the mariaDB to restart with all the changes
 # afterwards it will run with networking enabled and in foreground
 # so it will have the fake PID1 from the container
-/usr/bin/mariadb-admin shutdown --socket=/var/run/mysqld/mysqld.sock -u root
+# ------------------------------------------------------
+# once again simpler version is possible thankfully
+# rather than -> /usr/bin/mariadb-admin we got below one
+# ------------------------------------------------------ 
+mysqladmin shutdown --socket=/var/run/mysqld/mysqld.sock -u root
 
 # DEBUG FOR FINISHING THE MARIADB SETUP
 echo "---------------------------------------"
@@ -78,4 +105,8 @@ echo "---------------------------------------"
 
 # replacing the shell with mysqld process
 # making the change into PID1 as per above
-exec mysqld --user=mysql
+# ---------------------------------------
+# no need for option --user=mysql
+# the Debian supremacy for use is clear
+# --------------------------------------
+exec mysqld
--- src/req/wordpress/script.sh
+++ src/req/wordpress/script.sh
@@ -1,30 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
 # exit if non 0 value
 set -e
 
 # prevents wordpress cli from sending emails
-# and adding higher memory limit to PHP
 export WP_CLI_DISABLE_MAIL=true
-export WP_CLI_PHP_ARGS='-d memory_limit=512M'
+
+# and adding higher memory limit to PHP
+# ------------------------------------------
+# once again memory problem is not there now
+# ------------------------------------------
+# export WP_CLI_PHP_ARGS='-d memory_limit=512M'
 
 # ------------------------------------
 # Moving this part inside docker build
 # ------------------------------------
 # check if wordpress CLI is installed
 # if not download, makes executable and moves
-# if ! command -v wp > /dev/null; then
-# 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# 	chmod +x wp-cli.phar
-# 	mv wp-cli.phar /usr/local/bin/wp
-# fi
+# ------------------------------------------------------
+# And once again back to script no need for optimizaiton
+# -------------------------------------
+if ! command -v wp > /dev/null; then
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+fi
+
 # echo "Did we fail the command -v -> $?"
 
 # ---------------
 # DEBUG STATEMENT
 # ---------------
-ls -la
-echo "FIRST LISTING"
+# ls -la
+# echo "FIRST LISTING"
 
 # making all the important files readable
 cd /var/www/html
@@ -33,19 +41,31 @@ chmod -R 755 /var/www/html
 # ---------------
 # DEBUG STATEMENT
 # ---------------
-ls -la
-echo "SECOND LISTING"
+# ls -la
+# echo "SECOND LISTING"
 
 # [file name part just linke in DB setup]
-FILE=/etc/php84/php-fpm.d/www.conf
+# ---------------------------------------------------
+# that could have been a problem with my alpine setup
+# the file was there but I changed so much in between
+# FILE=/etc/php84/php-fpm.d/www.conf
+# --------------------------------------------------
+FILE=/etc/php/7.4/fpm/pool.d/www.conf
 
 # changing the PHP-FPM socket to use port 9000
-sed -i 's|^listen =.*|listen = 9000|' "$FILE"
+# -------------------------------------------
+# this also might need to be changed to
+# '36 s|/run/php/php7.4-fpm.sock|9000|'
+# ---------------------------------------------
+# sed -i 's|^listen =.*|listen = 9000|' "$FILE"
+# I did change it to it as per the following
+sed -i '36 s|/run/php/php7.4-fpm.sock|9000|' $FILE
 
 echo "---------------------------------------------"
 echo "do we get proper user $MYSQL_USER"
 echo "and what about password? $MYSQL_PASS"
 echo "---------------------------------------------"
+
 # ------------------------------------------
 # seems like something is wrong at this part
 # adding it for testing right now again
@@ -53,8 +73,12 @@ echo "---------------------------------------------"
 # checking the MariaDB if its ready
 # trying up to 10 times every 2 seconds
 # most likely to be removed if healthcheck works
+# -------------------------------------------------
+# Back at it most likely some alpine specific issue
+# for now replaces the healthcheck in compose file
+# -------------------------------------------------
 for i in $(seq 1 10); do
-	if mariadb -h mariadb -u "${MYSQL_USER}" \
+	if mariadb -h mariadb -P 3306 -u "${MYSQL_USER}" \
 		-p"${MYSQL_PASS}" -e "SELECT 1" &>/dev/null; then
 		break
 	else
@@ -72,29 +96,44 @@ fi
 # make a WordPress config file if it doesn't exist
 # adding a wraper to try and retry the connection
 # before fully exiting the script
-for i in $(seq 1 10); do
-	if [ ! -f wp-config.php ]; then
-		echo "Creating a wp-config file tried ${i}/10"
-		if wp config create \
-			--dbname="${MYSQL_DATABASE}" \
-			--dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASS}" \
-			--dbhost="mariadb:3306" --allow-root; then
-			echo "WE DID IT WP CONFIG CREATED"
-			break
-		else
-			echo "we are retrying the config"
-			sleep 2
-		fi
-	else
-		echo "wp-config exists skipping the creation"
-		break
-	fi
-done
+# ------------------------------------------
+# Going through prior checks we can simplify
+# for i in $(seq 1 10); do
+# 	if [ ! -f wp-config.php ]; then
+# 		echo "Creating a wp-config file tried ${i}/10"
+# 		if wp config create \
+# 			--dbname="${MYSQL_DATABASE}" \
+# 			--dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASS}" \
+# 			--dbhost="mariadb:3306" --allow-root; then
+# 			echo "WE DID IT WP CONFIG CREATED"
+# 			break
+# 		else
+# 			echo "we are retrying the config"
+# 			sleep 2
+# 		fi
+# 	else
+# 		echo "wp-config exists skipping the creation"
+# 		break
+# 	fi
+# done
+# -----------------------
+# which I find beautifull
+# ------------------------
+if [ ! -f wp-config.php ]; then
+	wp config create \
+	--dbname="${MYSQL_DATABASE}" \
+	--dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASS}" \
+	--dbhost="mariadb:3306" --allow-root && \
+	echo "WE DID IT WP CONFIG CREATED"
+fi
 
 # install wordpress if its not there
 # fillout all of the informations about it
 # with supression of warning email not found
 # last line would be replacing the --skip-email
+# --------------------------------------------
+# Here things stayed relatively the same
+# --------------------------------------
 if ! wp core is-installed --allow-root; then
 	echo "trying to install wordpress and set it up" && \
 	wp core install --url="${DOMAIN_NAME}" --title="${WP_TITLE}" \
@@ -105,13 +144,19 @@ fi
 
 # create a user if he doesn't exist
 if ! wp user get "${WP_USER}" --allow-root &>/dev/null; then
-	echo "adding a user to wordpress"
+	echo "adding a user to wordpress" && \
 	wp user create "${WP_USER}" "${WP_USER_EMAIL}" \
-		--user_pass="${WP_USER_PASS}" --allow-root
+		--user_pass="${WP_USER_PASS}" --role=author --allow-root
 fi
 
 # setup permissions for the www-data
 chown -R www-data:www-data /var/www/html
 
+mkdir -p /run/php
+
 # -F to run the background and ignore daemonization
-exec php-fpm84 -F
+# ---------------------------------
+# one place where alpine is shorter
+# ---------------------------------
+# exec php-fpm84 -F
+exec /usr/sbin/php-fpm7.4 -F
